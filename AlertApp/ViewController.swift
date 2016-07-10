9a6f533abcca546c1932e4a87576946225df3d30
Juan Morillo <juanmorillios@gmail.com>
2016-07-10 11:57:08 +0200

    AppAlert
    Aplicación que muestra diferentes tipos de alertas.

--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +17,26 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+ 
+        let alertViewController : UIAlertController = UIAlertController(title: "Mensaje Alerta", message: "Esto es una prueba de alerta", preferredStyle: .alert)
+        
+        
+        let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .destructive, handler: nil)
+        
+        
+        alertViewController.addAction(okAction)
+        
+        present(alertViewController, animated: true, completion: nil)
+        
+    
+        
     }
 
 
