3c2e24594dc12fb4119f9ac7e2e325caf91f3e3b
Juan Morillo <juanmorillios@gmail.com>
2016-07-10 11:59:58 +0200

    Update 2
    Update descripción of alert.

--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -24,15 +24,16 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         
- 
+        //Declaramos una constante mensaje de la alerta
         let alertViewController : UIAlertController = UIAlertController(title: "Mensaje Alerta", message: "Esto es una prueba de alerta", preferredStyle: .alert)
         
-        
+        //Creamos una acción de tipo OK
         let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .destructive, handler: nil)
         
-        
+        //Agregamos la acciób a la alerta
         alertViewController.addAction(okAction)
         
+        //Presentamos la alerta de una forma animada
         present(alertViewController, animated: true, completion: nil)
         
     
